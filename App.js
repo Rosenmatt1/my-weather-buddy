6d09424a44e77093512dd6f44ca43acf9aefafa9
Matthew Rosenberg <mateo@Matthews-MacBook-Pro-2.local>
2019-03-28 17:47:57 -0600

    Added blue and a Tiffany blue background color

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 
 export default class App extends Component {
   render() {
     return (
+
       <View style={styles.container}>
-      
+        <Text style={styles.title} >My Weather Buddy</Text>
       </View>
     );
   }
@@ -18,12 +17,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+    backgroundColor: '#C8F4F9',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
+    alignContent: 'flex-start',
     margin: 10,
   },
   instructions: {
