d3e366933160b0d70e3b511f04f34be78c5769a9
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-04 13:42:48 -0600

    Commented out all redux code in App.js

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import { Platform } from 'react-native'
 import { createStackNavigator, createAppContainer, } from 'react-navigation'
-import { Provider } from 'react-redux'
-import store from './src/components/store.js'
+// import { Provider } from 'react-redux'
+// import store from './src/components/store.js'
 
 import AccountDetails from './src/components/AccountDetails.js'
 import CreateAccount from './src/components/CreateAccount.js'
 import CreateAlert from './src/components/CreateAlert.js'
 import ViewAlerts from './src/components/ViewAlerts.js'
 import Login from './src/components/Login.js'
-import Post from './src/components/Post.js'
-import Postform from './src/components/Postform.js'
+// import Post from './src/components/Post.js'
+// import Postform from './src/components/Postform.js'
 
 class App extends Component {
   constructor(props) {
@@ -19,21 +19,21 @@ class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
+      // <Provider store={store}>
         <AppNavigator/>
-      </Provider>
+      // </Provider>
     )
   }
 }
 
 const AppNavigator = createStackNavigator({
-  postform: Postform,
-  post: Post,
   createAccount: CreateAccount,
   login: Login,
   createAlert: CreateAlert,
   viewAlerts: ViewAlerts,
   accountDetails: AccountDetails,
+  // postform: Postform,
+  // post: Post,
 })
 
 const AppContainer = createAppContainer(AppNavigator);
